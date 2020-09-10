256a59c5804991e5a72f9842c7713748d74bd9b0
gugfug <gugfug1@gmail.com>
2020-09-10 04:19:22 +0000

    fresh start

--- src/core/index.ts
+++
@@ -1,2 +0,0 @@
-export { Scene } from './scene';
-export { Viewport, createViewportById } from './viewport';
--- webpack/settings.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-
-module.exports = {
-  globalSettings: {
-    DEV_BUILD_FOLDER: path.resolve('dist'),
-    SOURCE_FOLDER: path.resolve('src'),
-  },
-};
